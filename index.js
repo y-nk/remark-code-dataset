3d3705b2e3f80f8c00151389f94c1df2a8f68c88
Julien Barbay <julien.barbay@gmail.com>
2023-01-17 12:23:03 +0700

    misc: minor adjustments towards json5

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import { visit } from "unist-util-visit";
-import { parse } from 'json5';
+import json5 from 'json5';
 
 export default function remarkCodeDataset(options) {
-  return function transform(tree) {
-    visit(tree, 'code', function (node) {
-      const attrs = parse(node.meta ?? "");
+  return function transform(tree, file) {
+    visit(tree, 'code', function(node) {
+      if (!node.meta) return
 
-      const {
-        exclude = [],
-        include = Object.keys(attrs)
-      } = options ?? {}
+      try {
+        const attrs = json5.parse(node.meta);
 
-      node.data ??= {}
+        const {
+          exclude = [],
+          include = Object.keys(attrs)
+        } = options ?? {}
 
-      node.data.hProperties = Object.fromEntries(
-        Object.entries(attrs)
-          .filter(([k]) => !exclude.includes(k))
-          .filter(([k]) => include.includes(k))
-          .map(([k, v]) => [`data-${k}`, v])
-      )
+        node.data ??= {}
+
+        node.data.hProperties = Object.fromEntries(
+          Object.entries(attrs)
+            .filter(([k]) => !exclude.includes(k))
+            .filter(([k]) => include.includes(k))
+            .map(([k, v]) => [`data-${k}`, v])
+        )
+      } catch {
+        if (process.env.NODE_ENV !== 'production') {
+          const [filename] = file.history
+          console.error(`[remark-code-dataset] Invalid JSON5 structure in ${filename}: ${node.meta}; ignoring.`)
+        }
+      }
     })
   }
 }
