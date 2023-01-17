ed415c479cdca50c73cefecdf3f3f04961b6a599
Julien Barbay <julien.barbay@gmail.com>
2023-01-17 10:19:56 +0700

    feat: use json5 as parser

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-import { parse } from "node:querystring";
 import { visit } from "unist-util-visit";
+import { parse } from 'json5';
 
-export default function remarkCodeDataset({
-  metaDelimiter = " ",
-  includeMetas = null,
-} = {}) {
+export default function remarkCodeDataset(options) {
   return function transform(tree) {
     visit(tree, 'code', function (node) {
-      const attrs = parse(node.meta ?? "", metaDelimiter);
+      const attrs = parse(node.meta ?? "");
+
+      const {
+        exclude = [],
+        include = Object.keys(attrs)
+      } = options ?? {}
 
       node.data ??= {}
 
       node.data.hProperties = Object.fromEntries(
         Object.entries(attrs)
-          .filter(([k]) => !includeMetas || includeMetas.includes(k))
+          .filter(([k]) => !exclude.includes(k))
+          .filter(([k]) => include.includes(k))
           .map(([k, v]) => [`data-${k}`, v])
       )
     })
